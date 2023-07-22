80d75fe1ccb983135622c249452152f608434262
ashish <ashishchoksi84@gmail.com>
2023-07-22 22:43:04 +0530

    Add more method to repository and integration test case for the same

--- src/main/java/com/example/springboot/repository/StudentRepository.java
+++ src/main/java/com/example/springboot/repository/StudentRepository.java
@@ -4,7 +4,16 @@ import com.example.springboot.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    List<Student> findByFirstname(String firstName);
+
+    List<Student> findByFirstnameContaining(String firstName);
+
+    List<Student> findByLastNameNotNull();
+
+    List<Student> findByGuardianName(String guardianName);
 }
--- src/test/java/com/example/springboot/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/springboot/repository/StudentRepositoryTest.java
@@ -1,13 +1,12 @@
 package com.example.springboot.repository;
 
+import com.example.springboot.entity.Guardian;
 import com.example.springboot.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest // this is use for integration test not recommended for JPA test because it impacts DB
 // @DataJpaTest // standard way to test repository
@@ -28,14 +27,56 @@ class StudentRepositoryTest {
         System.out.println("All students : "  + students);
     }
 
+    @Test
+    public void testStudentWithGuardian() {
+        Guardian guardian = Guardian.builder()
+                .name("chirag")
+                .email("chirag@mail.com")
+                .mobile("1234567809")
+                .build();
+
+        Student student = Student.builder()
+                .firstname("ashish")
+                .lastName("choksi")
+                .emailId("ash@info.com")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void testFindByFirstName() {
+        List<Student> students = studentRepository.findByFirstname("ashish");
+        System.out.println("Find by first name : " + students);
+    }
+
+    @Test
+    public void testFindByFirstNameContaining() {
+        List<Student> students = studentRepository.findByFirstnameContaining("as");
+        System.out.println("Find by first name with pattern: " + students);
+    }
+
+    @Test
+    public void testFindStudentWithGuardianName() {
+        List<Student> students = studentRepository.findByGuardianName("piyush");
+        System.out.println("Find by guardian name " + students);
+    }
+
+    @Test
+    public void testFindStudentByLastNameNotNull() {
+        List<Student> students = studentRepository.findByLastNameNotNull();
+        System.out.println("Find by last name not null: " + students);
+    }
+
     private Student createStudent() {
         return Student.builder()
                 .firstname("ashish")
                 .lastName("choksi")
                 .emailId("ashish@gmail.com")
-                .guardianName("piyush")
-                .guardianEmail("piyush@gmail.com")
-                .guardianMobile("1232467897")
+//                .guardianName("piyush")
+//                .guardianEmail("piyush@gmail.com")
+//                .guardianMobile("1232467897")
                 .build();
     }
 
